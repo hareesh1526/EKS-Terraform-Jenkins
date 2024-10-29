ff00b8cf889adf0f427eae8316dd86f063820770
hareesh1526 <hareesham.hsn@gmail.com>
2024-10-29 13:25:29 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,7 @@ locals {
   region = "ap-south-1"
   name   = "amonkincloud-cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["ap-south-1"]
+  azs      = ["ap-south-1a", "ap-south-1a"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
